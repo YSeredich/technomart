788bae7dfd2ef0d38b99d69587266fa23ff3ac5f
ÐYulia Seredich <yseredich@gmail.com>
2015-09-21 00:14:45 +0300

    add write us js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,25 @@ window.addEventListener("keydown", function(event) {
         }
     }
 });
+
+var writeOpen = document.querySelector(".open-writeus");
+var writePopup = document.querySelector(".modal-content-write-us");
+var writeClose = writePopup.querySelector(".modal-content-close");
+
+writeOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    writePopup.classList.add("modal-content-show");
+});
+
+writeClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    writePopup.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode == 27) {
+        if (writePopup.classList.contains("modal-content-show")) {
+            writePopup.classList.remove("modal-content-show");
+        }
+    }
+});
